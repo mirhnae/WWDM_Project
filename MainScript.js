be2d34e555fa12f5144b9c1ddf6badc75c332c92
mirhnae <mirhnae@gmail.com>
2023-03-10 13:51:24 -0500

    first commit

---
+++ MainScript.js
@@ -0,0 +1,34 @@
+function myFunction() {
+    console.log("The button was clicked!!!");
+}
+function myFunction()
+{
+    document.getElementById("myButton").innerHTML = "You clicked the button!"
+    document.getElementById("myButton").style.color = "Purple"
+}
+function myOverFunction()
+{
+    document.getElementById("myButton").style.color = "Red";
+}
+
+function myOutFunction()
+{
+   
+    document.getElementById("myButton").style.color = "Black";
+}
+
+function myFunction()
+{
+    document.getElementById("bodytext").style.backgroundColor="Yellow";
+     document.getElementById("centertext").style.fontSize = "3.5rem";
+     document.getElementById("centertext").style.textAlign = "center";
+     document.getElementById("like").style.fontSize = "2.5rem";
+     document.getElementById("like").style.color = "green";
+     document.getElementById("like").style.textAlign = "center"
+     document.getElementById("important").style.textAlign = "center";
+     document.getElementById("important").style.fontSize = "3rem";
+     document.getElementById("important").style.color = "Orange";
+     
+
+     
+}
